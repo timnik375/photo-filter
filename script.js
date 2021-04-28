74d0283169b293ad869a71ba0f4f021caf374620
Mikita Tsimashenka <timoshenkonikita@gmail.com>
2021-04-28 16:01:25 +0300

    feat: add next-btn functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const filter = document.querySelector('.filters');
 const resetBtn = document.querySelector('.btn-reset')
+const nextBtn = document.querySelector('.btn-next')
 
 function filterOverlay(target) {
 	const sizing = target.dataset.sizing || '';
@@ -13,10 +14,41 @@ filter.addEventListener('input', (e) => {
 	}
 })
 
-resetBtn.addEventListener('click', (e) => {
+resetBtn.addEventListener('click', () => {
 	document.querySelectorAll('input[type=range]').forEach((elem) => {
 		elem.value = elem.getAttribute('value')
 		elem.nextElementSibling.innerText = elem.value
 		filterOverlay(elem)
 	})
+});
+
+let pictureNum = 0;
+
+nextBtn.addEventListener('click', (e) => {
+	let picture = document.querySelector('img');
+	let hourCur = new Date().getHours();
+	let dayTime = '';
+	pictureNum++;
+
+	if (hourCur > 5 && hourCur < 12) {
+		dayTime = 'morning'
+	} else if (hourCur > 11 && hourCur < 18) {
+		dayTime = 'day'
+	} else if (hourCur > 17 && hourCur < 24) {
+		dayTime = 'evening'
+	} else if (hourCur >= 0  && hourCur < 6) {
+		dayTime = 'night'
+	}
+
+	if (pictureNum > 20) {
+		pictureNum = 1
+	}
+
+	if (pictureNum < 10) {
+		picture.setAttribute('src', `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}/0${pictureNum}.jpg`)
+	} else {
+		picture.setAttribute('src', `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}/${pictureNum}.jpg`)
+	}
 })
+
+
