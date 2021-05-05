786b5ef0d8dbf807523211dde352db90cc435f0c
Mikita Tsimashenka <timoshenkonikita@gmail.com>
2021-05-05 11:41:41 +0300

    feat: add fullscreen functionality

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const filter = document.querySelector('.filters');
 const resetBtn = document.querySelector('.btn-reset')
 const nextBtn = document.querySelector('.btn-next')
 const fileloadBtn = document.querySelector('.btn-load--input');
-let picture = document.querySelector('img');
 const downloadBtn = document.querySelector('.btn-save');
-let canvas = document.querySelector('canvas')
+const picture = document.querySelector('img');
+const canvas = document.querySelector('canvas');
 let pictureNum = 0;
-
+let filterValue = [];
+const btnFullscreen = document.querySelector('.fullscreen');
 
 function drawImage() {
 	const img = new Image();
@@ -16,58 +17,68 @@ function drawImage() {
 		canvas.width = img.width;
 		canvas.height = img.height;
 		const ctx = canvas.getContext("2d");
+		ctx.filter = `blur(${canvas.height/picture.height*filterValue[0]}px) invert(${filterValue[1]}%) sepia(${filterValue[2]}%) saturate(${filterValue[3]}%) hue-rotate(${filterValue[4]}deg)`;
 		ctx.drawImage(img, 0, 0);
 	};
+}
+
+function getFilterValue () {
+	filterValue = [];
+	document.querySelectorAll('output').forEach((elem) => {
+		filterValue.push(elem.innerText);
+	})
 }
 
 window.onload = drawImage;
 
 function filterOverlay(target) {
 	const sizing = target.dataset.sizing || '';
-	document.documentElement.style.setProperty(`--${target.name}`, `${target.value}${sizing}`)
+	document.documentElement.style.setProperty(`--${target.name}`, `${target.value}${sizing}`);
 }
 
 filter.addEventListener('input', (e) => {
 	if (e.target.matches('input')) {
-		e.target.nextElementSibling.innerText = e.target.value
-		filterOverlay(e.target)
+		e.target.nextElementSibling.innerText = e.target.value;
+		filterOverlay(e.target);
 	}
+	getFilterValue();
+	drawImage();
 })
 
 resetBtn.addEventListener('click', () => {
 	document.querySelectorAll('input[type=range]').forEach((elem) => {
-		elem.value = elem.getAttribute('value')
-		elem.nextElementSibling.innerText = elem.value
-		filterOverlay(elem)
+		elem.value = elem.getAttribute('value');
+		elem.nextElementSibling.innerText = elem.value;
+		filterOverlay(elem);
 	})
 });
 
-nextBtn.addEventListener('click', (e) => {
+nextBtn.addEventListener('click', () => {
 	let hourCur = new Date().getHours();
-	let dayTime = '';
+	let dayTime = null;
 	pictureNum++;
 
 	if (hourCur > 5 && hourCur < 12) {
-		dayTime = 'morning'
+		dayTime = 'morning';
 	} else if (hourCur > 11 && hourCur < 18) {
-		dayTime = 'day'
+		dayTime = 'day';
 	} else if (hourCur > 17 && hourCur < 24) {
-		dayTime = 'evening'
+		dayTime = 'evening';
 	} else if (hourCur >= 0  && hourCur < 6) {
-		dayTime = 'night'
+		dayTime = 'night';
 	}
 
 	if (pictureNum > 20) {
-		pictureNum = 1
+		pictureNum = 1;
 	}
 
 	if (pictureNum < 10) {
-		picture.setAttribute('src', `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}/0${pictureNum}.jpg`)
+		picture.setAttribute('src', `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}/0${pictureNum}.jpg`);
 	} else {
-		picture.setAttribute('src', `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}/${pictureNum}.jpg`)
+		picture.setAttribute('src', `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${dayTime}/${pictureNum}.jpg`);
 	}
 
-	drawImage()
+	drawImage();
 })
 
 fileloadBtn.addEventListener('change', () => {
@@ -75,10 +86,11 @@ fileloadBtn.addEventListener('change', () => {
 	const reader = new FileReader();
 	reader.onload = () => {
 		picture.src = reader.result;
-	}
+		drawImage();
+	};
 	reader.readAsDataURL(file);
-	fileloadBtn.value = '';
-})
+	fileloadBtn.value = null;
+});
 
 downloadBtn.addEventListener('click', () => {
 	let link = document.createElement('a');
@@ -86,7 +98,18 @@ downloadBtn.addEventListener('click', () => {
 	link.href = canvas.toDataURL();
 	link.click();
 	link.delete;
+});
+
+btnFullscreen.addEventListener('click', () => {
+	if (!document.fullscreenElement) {
+		document.documentElement.requestFullscreen();
+	} else {
+		document.exitFullscreen()
+	}
 })
+
+
+
 
 
 
